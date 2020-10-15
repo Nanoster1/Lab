4c7897e95c27970254f16d1042db953a4c6f710e
Небытов Даниил <danil2002.11@mail.ru>
2020-10-16 01:08:10 +0500

    Lab05 Redact2

--- Lab05 Factorial/Program.cs
+++ Lab05 Factorial/Program.cs
@@ -6,27 +6,27 @@ namespace Lab05_Factorial
 {
     class Program
     {
-        static void SetCursor(string conc, int y)                                       //Функция для курсора
+        static void SetCursor(string conc, int y)                                          //Функция для курсора
         {
             int x = Console.WindowWidth / 2 - conc.Length / 2;
             int y2 = Console.WindowHeight / 2 + y;
             Console.SetCursorPosition(x, y2);
         }
-        static void WriteString(char a, int b)                                          //Функция для множественного написания символов
+        static void WriteString(char a, int b)                                             //Функция для множественного написания символов
         {
             Console.Write(new string(a, b));
         }
 
 
 
-        static void Main()                                                               //Основа
+        static void Main()                                                                 //Основа
         {
             WriteGreeting();
             ulong number = Input();
             Loading();
             Output(number);
         }
-        static void WriteGreeting()                                                      //Приветствие
+        static void WriteGreeting()                                                        //Приветствие
         {
             string greeting = ("Здравствуйте, ваше число, пожалуйста: ");
             for (int i = 0; i < greeting.Length; i++)
@@ -35,14 +35,14 @@ namespace Lab05_Factorial
                 Thread.Sleep(50);
             }
         } 
-        static ulong Input()                                                              //Ввод
+        static ulong Input()                                                               //Ввод
         {
             string numberStr = Console.ReadLine();
             Check(numberStr);
             return ulong.Parse(numberStr);
         }
-        static void Check(string numberStr)                                               //Проверка на число
-        {
+        static void Check(string numberStr)                                                //Проверка на число
+        { 
             ulong a;
             bool checking = ulong.TryParse(numberStr, out a);
             while (checking == false)
@@ -50,7 +50,7 @@ namespace Lab05_Factorial
                 Main();
             }
         }
-        static void Loading()                                                             //Экран загрузки (Чисто декор)
+        static void Loading()                                                              //Экран загрузки (Чисто декор)
         {
             int i = 1;
             string load = "Loading";
@@ -64,30 +64,30 @@ namespace Lab05_Factorial
                 Thread.Sleep(600);
             }
         }
-        static void Output(ulong number)                                                  //Зацикленный вывод со сменой цвета
+        static void Output(ulong number)                                                   //Зацикленный вывод со сменой цвета
         {
-            string conc = ($"│$-$-$-$-$-$-$-$-$-$-$-$-$-$-$-$ |{Сalculation(number)}| $-$-$-$-$-$-$-$-$-$-$-$-$-$-$-$│");
+            string conc = ($"│$-$-$-$-$-$-$-$-$-$-$-$-$-$-$-$ |{Сalculate(number)}| $-$-$-$-$-$-$-$-$-$-$-$-$-$-$-$│");
                 Console.Clear();               
             while (true)
             {
-                IntoOutput(conc);
+                IsIntoOutput(conc);
                 SwapColors(Console.ForegroundColor, Console.BackgroundColor);
-                IntoOutput(conc);
+                IsIntoOutput(conc);
                 SwapColors(Console.ForegroundColor, Console.BackgroundColor);
             }
         }
-        static void IntoOutput(string conc)                                               //Табличка ("Внутренности вывода")
+        static void IsIntoOutput(string conc)                                              //Табличка ("Внутренности вывода")
         {
             Console.Clear();
             SetCursor(conc, -1);
-            DoubleIntoOut(conc, '┌', '┐');
+            IsDoubleIntoOut(conc, '┌', '┐');
             SetCursor(conc, 0);
             Console.Write(conc);
             SetCursor(conc, 1);
-            DoubleIntoOut(conc, '└', '┘');
+            IsDoubleIntoOut(conc, '└', '┘');
             Thread.Sleep(1000);          
         }
-        static void DoubleIntoOut(string conc, char leftSymbol, char rightSymbol)         //строки рамки                  
+        static void IsDoubleIntoOut(string conc, char leftSymbol, char rightSymbol)        //строки рамки                  
         {
             Console.Write(leftSymbol);
             WriteString('─', conc.Length - 2);
@@ -102,7 +102,7 @@ namespace Lab05_Factorial
             Console.ForegroundColor = f;
             Console.BackgroundColor = b;
         }
-        static ulong Сalculation(ulong number)                                             //Вычисление факториала
+        static ulong Сalculate(ulong number)                                               //Вычисление факториала
         {       
             ulong fact = 1;
             for(ulong i = 1; i <= number; i++)
